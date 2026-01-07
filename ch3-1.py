eaa969e458a3291da7c1fa097127f17609ba9f36
youngchan124-arch <youngchan124@tukorea.ac.kr>
2026-01-07 18:02:48 +0900

    Add files via upload

---
+++ ch3-1.py
@@ -0,0 +1,19 @@
+'''
+print (1 + 1)
+print (3 - 2)
+print (5 * 2)
+print (6 // 3)
+
+print(2 ** 3)
+print(10 % 3) # %는 나머지
+print(10 // 3) # //는 몫 구하기
+'''
+print( 10 > 3)
+print( 4 >= 7)
+print( 10 < 3)
+print( 5 <= 5)
+
+print ( 3 == 3)
+print ( 4 == 2)
+print ( 3 + 4 == 7)
+print (1 != 3) # '!='시 왼쪽 오른쪽 다르다.
